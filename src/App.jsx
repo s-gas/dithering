8315e956bf95290804ec78a8082283ee13f99b2a
Simone Gasparini <simone.gasparini.2@gmail.com>
2026-07-07 11:35:19 +0200

    Render canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import { useState } from 'react'
+import { useRef, useEffect } from 'react'
 
 function App() {
-  return <img src="/pic.jpg" />
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    const img = new Image();
+    img.onload = () => {
+      canvas.width = 600;
+      canvas.height = img.height * canvas.width / img.width;
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    }
+    img.src = "/pic.jpg";
+  }, [])
+
+  return <canvas ref={canvasRef} />
 }
 
 export default App
